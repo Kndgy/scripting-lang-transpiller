25be17a1c5c3db077841f9e01a482dd5a04f8e0b
Rafli Ghani <ghanirafli8@gmail.com>
2022-12-11 16:38:15 +0700

    lefthand expression parser

--- frontend/parser.ts
+++ frontend/parser.ts
@@ -17,6 +17,16 @@ export default class Parser {
     return prev;
   }
 
+  private expect (type: TokenType, err: any) {
+    const prev = this.tokens.shift() as Token;
+    if(!prev || prev.type != type){
+      console.error("Parser Error:\n", err, prev, "- Expecting: ", type);
+      Deno.exit(1)
+    }
+
+    return prev
+  }
+
   public produceAST(sourccode: string): Program {
     this.tokens = tokenize(sourccode);
     const program: Program = {
@@ -38,8 +48,55 @@ export default class Parser {
   } 
 
   private parse_expr(): Expr {
-    return this.parse_primary_expr();
+    return this.parse_additive_expr();
   } 
+
+  // simply get parse left hand expr, right side returned as object
+  private parse_additive_expr(): Expr {
+    let left = this.parse_multiplicative_expr();
+
+    while(this.at().value == "+" || this.at().value == "-") {
+      const operator = this.eat().value;
+      const right = this.parse_multiplicative_expr();
+      left = {
+        kind: "BinaryExpr",
+        left, 
+        right,
+        operator,
+      } as BinaryExpr;
+    }
+
+    return left;
+  }
+
+  private parse_multiplicative_expr(): Expr {
+    let left = this.parse_primary_expr();
+
+    while(this.at().value == "/" || this.at().value == "*" || this.at().value == "%") {
+      const operator = this.eat().value;
+      const right = this.parse_primary_expr();
+      left = {
+        kind: "BinaryExpr",
+        left, 
+        right,
+        operator,
+      } as BinaryExpr;
+    }
+
+    return left;
+  }
+
+  // Orders of Presidence
+  //
+  // AssigntmentExpr
+  // MemberExpr
+  // functionalCall
+  // LogicalExpr
+  // ComparisonExpr
+  // AdditiveExpr
+  // MultiplicativeExpr
+  // UnaryExpr
+  // PrimaryExpr
 
   private parse_primary_expr(): Expr {
     const token = this.at().type;
@@ -53,7 +110,12 @@ export default class Parser {
         return { 
           kind: "NumericLiteral", 
           value: parseFloat(this.eat().value) } as NumericLiteral;
-        
+      case TokenType.OpenParen:{
+        this.eat();
+        const value = this.parse_expr();
+        this.expect(TokenType.CloseParen, "Unexpected token found inside parenthesised expression. Expecting closing parenthesis.",);
+        return value;
+      }
       default: 
         console.error("Unexpected token found during parsing", this.at())
         Deno.exit(1)
